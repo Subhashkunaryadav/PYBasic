6c8c1e816f0d08d73f86fd9833f71ffb39cf5cf9
subhash kumar yadav <110653851+Subhashkunaryadav@users.noreply.github.com>
2022-11-29 11:16:33 +0530

    Add files via upload

---
+++ print (3 + 5).py
@@ -0,0 +1,62 @@
+print (3 + 5)
+print (3 -5)
+print (3**5)
+
+#strings
+name = "subhash kumar yadav"
+print (name)
+print (name.upper())
+print (name.lower())
+first_name = "subhash"
+last_name = "kumar"
+print (first_name + " " +last_name)
+
+#\n\t
+print ("python")
+print ("\tpython")
+print ("\npython")
+print ("python")
+print ("courses; \n\tBCA  \n\tBCOM \n\tBTEC  \n\tBSC")
+message = "This is the bca and it's intresting"
+print (message)
+
+message = " Hi Eric, would you like to learn python"
+print (message)
+
+
+first_name = "subhash"
+last_name = "yadav"
+print (first_name + " " + last_name)
+name = "subhash kumar"
+print (name.title())
+print (name.upper())
+print (name.lower())
+
+#Quotes
+Quotes = '\tAlbert Einstein once said, "A person who never made a \n\tmistake never tried anything new"'
+print (Quotes)
+
+#Numbers
+print (2 + 3)
+print ( 2 - 3)
+print ( 2 * 3)
+print ( 2 ** 3)
+print ( 2 + 3 * 4)
+print ( 2.10 + 3.0)
+print ( 0.2 + 0.1)
+
+#str functions
+age = 23
+message = " Happy " + str(age) + "rd Birthday"
+print (message)
+
+#integer
+print ( 5+3)
+print (10-2)
+print (2*4)
+print (2**3)
+
+#Favorite number
+number = 10
+message = (" my favorite number is " + str(number))
+print (message)
